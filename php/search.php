3c720707126e201eb348085a22d988b4ecca045f
Mamba1099 <mambakaranja240@gmail.com>
2023-09-17 13:00:54 +0300

    user search functionality

---
+++ php/search.php
@@ -0,0 +1,16 @@
+<?php
+    session_start();
+    include_once "config.php";
+
+    $outgoing_id = $_SESSION['unique_id']; // Getting logged in user id
+    $searchTerm = mysqli_real_escape_string($conn, $_POST['searchTerm']); // Getting search term
+
+    $sql = "SELECT * FROM users WHERE NOT unique_id = {$outgoing_id} AND (fname LIKE '%{$searchTerm}%' OR lname LIKE '%{$searchTerm}%') "; // Getting all users except logged in user
+    $output = "";
+    $query = mysqli_query($conn, $sql); // Executing query
+    if(mysqli_num_rows($query) > 0){ 
+        include_once "data.php"; 
+    }else{
+        $output .= 'No user found related to your search term';
+    }
+    echo $output;
