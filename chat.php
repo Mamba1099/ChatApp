e816f4f63f56696ec7a566e3913706e67b81f631
Mamba1099 <mambakaranja240@gmail.com>
2023-09-18 14:07:46 +0300

    lint:used correct php format

--- chat.php
+++ chat.php
@@ -1,29 +1,55 @@
 <?php
+
+/**
+ * File: chat.php
+ *
+ * This file handles the ChatApp functionality.
+ *
+ * PHP version 7
+ *
+ * @category ChatApp
+ * @package  ChatApp
+ * @author   Sammy Karanja <mambakaranja240@gmail.com>
+ * @license  MIT License
+ * @link     https://@localhost:8080/chat.php
+ */
+
 session_start();
-include_once "php/config.php";
-if(!isset($_SESSION['unique_id'])){
-header("location: login.php");
+require_once "php/config.php";
+
+if (!isset($_SESSION['unique_id'])) {
+    header("location: login.php");
+
 }
 ?>
-<?php include_once "header.php"; ?>
+
+<?php require_once "header.php"; ?>
 
 <body>
   <div class="wrapper">
     <section class="chat-area">
       <header>
         <?php
-        $user_id = mysqli_real_escape_string($conn, $_GET['user_id']); // Getting user id from url
-        $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$user_id}"); // Getting user data from database
+        $user_id = mysqli_real_escape_string(
+            $conn,
+            $_GET['user_id']
+        ); // Getting user id from url
+        $sql = mysqli_query(
+            $conn,
+            "SELECT * FROM users WHERE unique_id = {$user_id}"
+        );
         if (mysqli_num_rows($sql) > 0) { // Checking if user data is there or not
-          $row = mysqli_fetch_assoc($sql); // Fetching user data
+            $row = mysqli_fetch_assoc($sql); // Fetching user data
         } else {
-          header("location: users.php");
+            header("location: users.php");
         }
         ?>
         <a href="users.php" class="back-icon"><i class="fas fa-arrow-left"></i></a>
         <img src="php/images/<?php echo $row['img']; ?>" alt="">
         <div class="details">
-          <span><?php echo $row['fname'] . " " . $row['lname'] ?></span> <!-- Displaying user full name -->
+          <span><?php echo $row[
+            'fname'] . " " . $row['lname'] ?>
+            </span> <!-- Displaying user full name -->
           <p><?php echo $row['status']; ?></p>
         </div>
       </header>
@@ -31,8 +57,16 @@ header("location: login.php");
 
       </div>
       <form action="#" class="typing-area">
-        <input type="text" class="incoming_id" name="incoming_id" value="<?php echo $user_id; ?>" hidden> <!-- Passing receiver user id -->
-        <input type="text" name="message" class="input-field" placeholder="Type a message here..." autocomplete="off">
+        <input type="text"
+               class="incoming_id" 
+               name="incoming_id" 
+               value="<?php echo $user_id; ?>" hidden> 
+               <!-- Passing receiver user id -->
+        <input type="text"
+               name="message" 
+               class="input-field" 
+               placeholder="Type a message here..." 
+               autocomplete="off">
         <button><i class="fab fa-telegram-plane"></i></button>
       </form>
     </section>
